1c2ddab8b638f333987fd56f1caae90181566d59
Kaser <Kaser@students.uni-marburg.de>
2024-09-29 07:49:20 +0200

    Simple routes in Django

--- HumanResource/views.py
+++ HumanResource/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+
+def home(request):
+    return HttpResponse('This is the home page')
+
+
+def about(request):
+    return HttpResponse('This is the about us page')
+
+def services(request):
+    return HttpResponse('This is the services page')
